e6c66f6baa57e2fa3c36992aa3411e27b1135f44
AlexandrVinogradov <ale.grapes.webdev@gmail.com>
2020-03-10 22:18:18 +0300

    start project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import style from './App.module.scss'
+import Login from './Login/Login'
+
+const App = () => {
+  return <div className={style.App}>
+    <Login />
+  </div>
+}
+
+export default App
---
+++ src/Login/Login.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import style from './Login.module.scss'
+import shield from '../img/shield.png'
+
+const Login = () => {
+  return (
+    <div className={style.Login}>
+      <img className={style.Login__shield} src={shield} alt='shield'/>
+      <span className={style.Login__title}>SPassword - Your safe</span>
+      <input />
+      <input />
+      <button className={style.Login__btn} type='button'>Login</button>
+    </div>
+  )
+}
+
+export default Login
