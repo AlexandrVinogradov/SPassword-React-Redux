37024df827b88aa847b6e62313738d09d70fe05a
AlexandrVinogradov <ale.grapes.webdev@gmail.com>
2020-03-11 21:10:22 +0300

    added Main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import { Route, BrowserRouter, Switch } from 'react-router-dom'
 import style from './App.module.scss'
 import Login from './Login/Login'
+import Main from './Main/Main'
 
 const App = () => {
-  return <div className={style.App}>
-    <Login />
-  </div>
+  return (
+    <div className={style.App}>
+      <Login />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/main" render={() => <Main />} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  )
 }
 
 export default App
---
+++ src/Main/Main.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import style from './Main.module.scss'
+
+const Main = () => {
+  return <div className={style.Main}>123</div>
+}
+
+export default Main
