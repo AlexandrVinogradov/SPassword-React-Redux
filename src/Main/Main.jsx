86fec559341e7a95d5052e2ea9cb5cbcf9c97e7d
AlexandrVinogradov <ale.grapes.webdev@gmail.com>
2020-03-12 21:00:06 +0300

    start layout of main component

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
 import style from './Main.module.scss'
+import shield from '../img/shield.png'
+import plus from '../img/Icon-plus.png'
+import arrow from '../img/icon-arrow.png'
+import NavListbar from './NavListbar/NavListbar'
 
 const Main = () => {
-  return <div className={style.Main}>123</div>
+  return (
+    <main className={style.Main}>
+      <section>
+        <div>
+          <img src={shield} alt="SPassword logo" />
+          <p>SPassword</p>
+        </div>
+
+        <div>
+          <p>Add group</p>
+          <img src={plus} alt="SPassword logo" />
+        </div>
+
+        <NavListbar />
+
+        <div>
+          <p>Logput</p>
+          <img src={arrow} alt="SPassword logo" />
+        </div>
+      </section>
+      
+      <section>2</section>
+    </main>
+  )
 }
 
 export default Main
---
+++ src/Main/NavListbar/NavListbar.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import style from './NavListbar.module.scss'
+
+const NavListbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/main" activeClassName={style.active} className={style.a}>
+            <div className={style.name} data-text={style.home}>
+              Vk
+            </div>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/main" activeClassName={style.active} className={style.a}>
+            <div className={style.name} data-text={style.home}>
+              Github
+            </div>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/main" activeClassName={style.active} className={style.a}>
+            <div className={style.name} data-text={style.home}>
+              Facebook
+            </div>
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default NavListbar
