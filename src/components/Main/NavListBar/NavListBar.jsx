f0e0a9227204cfbed6200e92836d59cd41408196
AlexandrVinogradov <ale.grapes.webdev@gmail.com>
2020-04-28 23:55:25 +0300

    render trouble

--- src/components/Main/NavListBar/NavListBar.jsx
+++ src/components/Main/NavListBar/NavListBar.jsx
@@ -3,38 +3,38 @@ import { NavLink } from 'react-router-dom'
 import style from './NavListBar.module.scss'
 
 const NavListBar = props => {
-  // console.log(props.groups)
-  // return (
-  //   <nav>
-  //     <ul className={style.NavListBar_ul}>
-  //       <li>
-  //         <NavLink to="/main/vk" activeClassName={style.active} className={style.a}>
-  //           Vk
-  //         </NavLink>
-  //       </li>
-  //       <li>
-  //         <NavLink to="/main/github" activeClassName={style.active} className={style.a}>
-  //           Github
-  //         </NavLink>
-  //       </li>
-  //       <li>
-  //         <NavLink to="/main/facebook" activeClassName={style.active} className={style.a}>
-  //           Facebook
-  //         </NavLink>
-  //       </li>
-  //       <li>
-  //         <NavLink to="/main/steam" activeClassName={style.active} className={style.a}>
-  //           Steam
-  //         </NavLink>
-  //       </li>
-  //       <li>
-  //         <NavLink to="/main/la2" activeClassName={style.active} className={style.a}>
-  //           La2 Accounts
-  //         </NavLink>
-  //       </li>
-  //     </ul>
-  //   </nav>
-  // )
+  console.log(props.groups)
+  return (
+    <nav>
+      <ul className={style.NavListBar_ul}>
+        <li>
+          <NavLink to="/main/vk" activeClassName={style.active} className={style.a}>
+            Vk
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/main/github" activeClassName={style.active} className={style.a}>
+            Github
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/main/facebook" activeClassName={style.active} className={style.a}>
+            Facebook
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/main/steam" activeClassName={style.active} className={style.a}>
+            Steam
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/main/la2" activeClassName={style.active} className={style.a}>
+            La2 Accounts
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  )
   const groups = props.groups.map(g => <Group groups={g} key={g.id} />)
   return <div>
     {groups}
