28d4140f2f8d7294763287f9859e08b97bc7c135
AlexandrVinogradov <ale.grapes.webdev@gmail.com>
2020-05-07 21:11:37 +0300

    added redux-form

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { reducer as formReducer } from 'redux-form'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import thunkMiddleware from 'redux-thunk'
 import mainReducer from './main-reducer'
 
 const reducers = combineReducers({
   mainPage: mainReducer,
+  form: formReducer,
 })
 
 const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunkMiddleware)))
