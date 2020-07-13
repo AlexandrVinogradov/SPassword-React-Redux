6d7c287aae4b9852f1f435d6e1d98e8bc713b02f
AlexandrVinogradov <ale.grapes.webdev@gmail.com>
2020-07-13 18:04:15 +0300

    start rewrite on ts

--- src/redux/main-reducer.ts
+++ src/redux/main-reducer.ts
@@ -5,18 +5,32 @@ const DELETE_GROUP = 'spassword/main/DELETE_GROUP'
 const SELECT_GROUP = 'spassword/main/SELECT_GROUP'
 const UPDATE_LOGIN = 'spassword/main/UPDATE_LOGIN'
 
+// export type GroupsType = {
+//   id: number,
+//   name: string | null,
+//   login: string,
+//   password: string,
+// }
+
+// export type InitialStateType = {
+//   groups: GroupsType,
+//   idOfSelectedGroup: number,
+// }
+
 const initialState = {
   groups: [
     { id: 0, name: 'Vk', login: 'admin', password: '12345' },
     { id: 1, name: 'Github', login: 'hello', password: 'qweasdzxc' },
     { id: 2, name: 'Facebook', login: '123123', password: '1997' },
     { id: 3, name: 'Steam', login: 'monkey', password: 'love my mom' },
-    { id: 4, name: 'La2 Accounts', login: 'password', password: 'password' }
+    { id: 4, name: 'La2 Accounts', login: 'password', password: 'password' },
   ],
   idOfSelectedGroup: 0,
 }
 
-const mainReducer = (state = initialState, action) => {
+// export type InitialStateType = typeof initialState
+
+const mainReducer = (state = initialState, action: any) => {
   switch (action.type) {
     case ADD_GROUP:
       return {
@@ -38,7 +52,7 @@ const mainReducer = (state = initialState, action) => {
     case UPDATE_LOGIN:
       return {
         // ...state.groups,
-        groups: newLogin(state.groups, state.idOfSelectedGroup, action.login, action.password),
+        groups: newLogin(state.groups, state.idOfSelectedGroup, action.login),
       }
 
     default:
@@ -48,7 +62,7 @@ const mainReducer = (state = initialState, action) => {
 
 export default mainReducer
 
-export const addGroup = name => ({ type: ADD_GROUP, name })
-export const deleteGroup = name => ({ type: DELETE_GROUP, name })
-export const selectGroup = idOfSelectedGroup => ({ type: SELECT_GROUP, idOfSelectedGroup })
-export const updateLogin = login => ({ type: UPDATE_LOGIN, login })
+export const addGroup = (name: any) => ({ type: ADD_GROUP, name })
+export const deleteGroup = (name: any) => ({ type: DELETE_GROUP, name })
+export const selectGroup = (idOfSelectedGroup: any) => ({ type: SELECT_GROUP, idOfSelectedGroup })
+export const updateLogin = (login: any) => ({ type: UPDATE_LOGIN, login })
