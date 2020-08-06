1b240d435e61b452db60fd9ba2356b8f995a589f
AlexandrVinogradov <ale.grapes.webdev@gmail.com>
2020-08-06 22:27:59 +0300

    Updates

---
+++ precache-manifest.48b7acb9837392fc91d6a22ad6aef31f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bf7de7329d9e72eef6b0e786aa539aa",
+    "url": "/SPassword-React-Redux/index.html"
+  },
+  {
+    "revision": "0a5522ca277991643bd7",
+    "url": "/SPassword-React-Redux/static/css/main.356d8fd1.chunk.css"
+  },
+  {
+    "revision": "383bdec13297efa511b3",
+    "url": "/SPassword-React-Redux/static/js/2.0afc1f6e.chunk.js"
+  },
+  {
+    "revision": "d847f44f892623762a0ee632f754942a",
+    "url": "/SPassword-React-Redux/static/js/2.0afc1f6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a5522ca277991643bd7",
+    "url": "/SPassword-React-Redux/static/js/main.3d9ec27a.chunk.js"
+  },
+  {
+    "revision": "aad6d52fae68ec1f1d02",
+    "url": "/SPassword-React-Redux/static/js/runtime-main.3f67133a.js"
+  },
+  {
+    "revision": "0c12ee2fd868459a1db633259b2ba50a",
+    "url": "/SPassword-React-Redux/static/media/Montserrat-Bold.0c12ee2f.ttf"
+  },
+  {
+    "revision": "242780083d90e494fa8a5f41a6a4d2cd",
+    "url": "/SPassword-React-Redux/static/media/Montserrat-Bold.24278008.woff"
+  },
+  {
+    "revision": "35b1efc06970f9de3ea323ab868133a1",
+    "url": "/SPassword-React-Redux/static/media/Montserrat-Bold.35b1efc0.woff2"
+  },
+  {
+    "revision": "42198212b25644482557cfb156f4893e",
+    "url": "/SPassword-React-Redux/static/media/Montserrat-Bold.42198212.eot"
+  },
+  {
+    "revision": "4d0275eccd484328b508605779da8960",
+    "url": "/SPassword-React-Redux/static/media/Montserrat-Regular.4d0275ec.eot"
+  },
+  {
+    "revision": "6775c6352916b278fba11055c253eb28",
+    "url": "/SPassword-React-Redux/static/media/Montserrat-Regular.6775c635.woff2"
+  },
+  {
+    "revision": "d8efdf82ac6b41c7eb8a360cbf62f523",
+    "url": "/SPassword-React-Redux/static/media/Montserrat-Regular.d8efdf82.ttf"
+  },
+  {
+    "revision": "ee1fbd6210a419d49649d45fcde6e62a",
+    "url": "/SPassword-React-Redux/static/media/Montserrat-Regular.ee1fbd62.woff"
+  }
+]);
